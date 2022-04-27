8066a8e544e327849a9eb2f5ea8e782f970bf973
mike <muhammedjepbarov@gmail.com>
2022-04-27 13:38:09 +0500

    updated the admin and template

--- main/models/Contact.py
+++ main/models/Contact.py
@@ -10,10 +10,51 @@ class Contact(BaseModel, db.Model):
 	page_id = db.Column("page_id",db.Integer,db.ForeignKey("tbl_page.id"))
 	text_data_id = db.Column("text_data_id",db.Integer,db.ForeignKey("tbl_text_data.id"))
 
+	phone_number = db.Column("phone_number",db.String)
+	home_phone_number = db.Column("home_phone_number",db.String)
+	work_phone_number = db.Column("work_phone_number",db.String)
+	phone_number_1 = db.Column("phone_number_1",db.String)
+	phone_number_2 = db.Column("phone_number_2",db.String)
+	phone_number_3 = db.Column("phone_number_3",db.String)
+	mail = db.Column("mail",db.String)
+	mail_1 = db.Column("mail_1",db.String)
+	mail_2 = db.Column("mail_2",db.String)
+	mail_3 = db.Column("mail_3",db.String)
+	address = db.Column("address",db.String)
+	address_1 = db.Column("address_1",db.String)
+	address_2 = db.Column("address_2",db.String)
+	address_3 = db.Column("address_3",db.String)
+	social_1_name = db.Column("social_1_name",db.String)
+	social_1 = db.Column("social_1",db.String)
+	social_2_name = db.Column("social_2_name",db.String)
+	social_2 = db.Column("social_2",db.String)
+	social_3_name = db.Column("social_3_name",db.String)
+	social_3 = db.Column("social_3",db.String)
+
 	def to_json_api(self):
 		data = {
 			"id": self.id,
 			"page_id": self.page_id,
+			"phone_number": self.phone_number,
+			"home_phone_number": self.home_phone_number,
+			"work_phone_number": self.work_phone_number,
+			"phone_number_1": self.phone_number_1,
+			"phone_number_2": self.phone_number_2,
+			"phone_number_3": self.phone_number_3,
+			"mail": self.mail,
+			"mail_1": self.mail_1,
+			"mail_2": self.mail_2,
+			"mail_3": self.mail_3,
+			"address": self.address,
+			"address_1": self.address_1,
+			"address_2": self.address_2,
+			"address_3": self.address_3,
+			"social_1_name": self.social_1_name,
+			"social_1": self.social_1,
+			"social_2_name": self.social_2_name,
+			"social_2": self.social_2,
+			"social_3_name": self.social_3_name,
+			"social_3": self.social_3,
 		}
 		for key, value in BaseModel.to_json(self).items():
 			data[key] = value
--- main/web/utils.py
+++ main/web/utils.py
@@ -7,8 +7,22 @@ def get_all_data():
 	for page in pages:
 		page_data = page.to_json_api()
 		page_data["images"] = [image.to_json_api() for image in page.Image]
-		page_data["categories"] = [category.to_json_api() for category in page.Category]
-		# page_data["collections"] = [collection.to_json_api() for collection in page.Collection]
+		page_data["contacts"] = [contact.to_json_api() for contact in page.Contact]
+
+		categories_list = []
+		for category in page.Category:
+			category_data = category.to_json_api()
+			collections_list = []
+			for collection in category.Collection:
+				collection_data = collection.to_json_api()
+				collection_data["category_name"] = collection.category.name if collection.category else ''
+				collection_data["images"] = [image.to_json_api() for image in collection.Image]
+				collections_list.append(collection_data)
+				category_data["collections"] = collections_list
+
+			categories_list.append(category_data)
+		
+		page_data["categories"] = categories_list
 
 		collections_list = []
 		for collection in page.Collection:
@@ -18,6 +32,7 @@ def get_all_data():
 			collections_list.append(collection_data)
 		
 		page_data["collections"] = collections_list
+
 
 		data[page.name] = page_data
 
